0f5ff8261f692bfcdbab016f607aa74966207ea0
LarsFlaeten <lars.flaeten@gmail.com>
2025-02-12 14:23:20 +0100

    Add all the tests..

--- test/testInstructionSuite.cpp
+++ test/testInstructionSuite.cpp
@@ -8,7 +8,7 @@
 
 #include <cmath>
 
-std::string path = "../test/asm/";
+std::string path = "../../test/asm/";
 
 
 class INSTRTest : public ::testing::Test {
@@ -29,6 +29,24 @@ protected:
     SDRAM<0x01000000> RAM;   // IO: 0xf0000000, 16 MB of RAM
  
     CPU cpu;
+    
+    
+    void do_test_assertg7(const std::string& test) {
+        // Read the ELF and get the entry point, then reset
+        u32 entry_va = 0x0; 
+        u32 word_count = ReadElf(path + test, cpu, entry_va); 
+        ASSERT_GT(word_count, 0);
+        cpu.Reset(entry_va);
+        
+        RunSummary rs;
+        cpu.Run(0, &rs); 
+
+        // Check if test hac placed GOOD (0x900d) or BAD (0xbad) in %g7
+        u32 val; cpu.ReadReg(GLOBALREG7, &val);
+        ASSERT_EQ(val, 0x900d); 
+
+
+    }
 };
 
 
@@ -37,7 +55,7 @@ INSTRTest::INSTRTest()
     : cpu()
 
 {  
-    cpu.SetVerbose(true);
+    //cpu.SetVerbose(true);
     cpu.SetId(0);
 
     // Set up IO mapping
@@ -67,15 +85,115 @@ void INSTRTest::TearDown()
 {
 }
 
+
 TEST_F(INSTRTest, add)
 {
-
-    // Read the ELF and get the entry point, then reset
-    u32 entry_va = 0x0; 
-    u32 word_count = ReadElf(path + "add.aout", cpu, entry_va); 
-    ASSERT_GT(word_count, 0);
-    cpu.Reset(entry_va);
-    
-    RunSummary rs;
-    cpu.Run(0, &rs); 
+    do_test_assertg7("add.aout");
 } 
+
+TEST_F(INSTRTest, _and)
+{
+    do_test_assertg7("and.aout");
+} 
+
+TEST_F(INSTRTest, bicc)
+{
+    do_test_assertg7("bicc.aout");
+} 
+
+TEST_F(INSTRTest, call)
+{
+    do_test_assertg7("call.aout");
+} 
+
+TEST_F(INSTRTest, div)
+{
+    do_test_assertg7("div.aout");
+} 
+
+TEST_F(INSTRTest, jmpl)
+{
+    do_test_assertg7("jmpl.aout");
+} 
+
+TEST_F(INSTRTest, ld)
+{
+    do_test_assertg7("ld.aout");
+} 
+
+TEST_F(INSTRTest, ldstub)
+{
+    do_test_assertg7("ldstub.aout");
+} 
+
+TEST_F(INSTRTest, mul)
+{
+    do_test_assertg7("mul.aout");
+} 
+
+TEST_F(INSTRTest, mulscc)
+{
+    do_test_assertg7("mulscc.aout");
+} 
+
+TEST_F(INSTRTest, _or)
+{
+    do_test_assertg7("or.aout");
+} 
+
+TEST_F(INSTRTest, rd_wr)
+{
+    do_test_assertg7("rd_wr.aout");
+} 
+
+TEST_F(INSTRTest, save_rest)
+{
+    do_test_assertg7("save_rest.aout");
+} 
+
+TEST_F(INSTRTest, sethi)
+{
+    do_test_assertg7("sethi.aout");
+} 
+
+TEST_F(INSTRTest, shift)
+{
+    do_test_assertg7("shift.aout");
+} 
+
+TEST_F(INSTRTest, st)
+{
+    do_test_assertg7("st.aout");
+} 
+
+TEST_F(INSTRTest, sub)
+{
+    do_test_assertg7("sub.aout");
+} 
+
+TEST_F(INSTRTest, swap)
+{
+    do_test_assertg7("swap.aout");
+} 
+
+TEST_F(INSTRTest, tadd)
+{
+    do_test_assertg7("tadd.aout");
+} 
+
+TEST_F(INSTRTest, ticc)
+{
+    do_test_assertg7("ticc.aout");
+} 
+
+TEST_F(INSTRTest, tsub)
+{
+    do_test_assertg7("tsub.aout");
+} 
+
+TEST_F(INSTRTest, _xor)
+{
+    do_test_assertg7("xor.aout");
+} 
+
+
