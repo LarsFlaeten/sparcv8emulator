64b9c230416abbd5b1121a7d50bb6c2e9ee3b3da
LarsFlaeten <lars.flaeten@gmail.com>
2025-02-12 14:10:59 +0100

    Update with new scaler values

--- test/testGPTIMER.cpp
+++ test/testGPTIMER.cpp
@@ -56,7 +56,7 @@ void GPTIMERTest::TearDown()
 
 TEST_F(GPTIMERTest, TimerEnable)
 {
-    GPTIMER timer;
+    GPTIMER timer(8, 0xff);
     
     ASSERT_EQ(timer.VendorId(), VENDOR_GAISLER );
     ASSERT_EQ(timer.DeviceId(), GAISLER_GPTIMER );
@@ -76,29 +76,29 @@ TEST_F(GPTIMERTest, TimerEnable)
 
 TEST_F(GPTIMERTest, TimerTickOnce)
 {
-    GPTIMER timer;
+    GPTIMER timer(8, 0x31);
    
     // Tick through the prescaler so that timer 1 counter is decremented 
-    for(int i = 0; i <= 256; ++i)
+    for(int i = 0; i <= 0x31; ++i)
         timer.Tick();
 
     u32 cnt = timer.Read(0x10);
     u32 rld = timer.Read(0x14);
     u32 ctrl = timer.Read(0x18);
 
-    ASSERT_EQ(rld ,0xffffffff);
-    ASSERT_EQ(cnt ,0xfffffffe); // rld minus one
+    ASSERT_EQ(rld ,0x270f);
+    ASSERT_EQ(cnt ,0x270e); // rld minus one
     ASSERT_EQ(ctrl & 0x1, 0x1);
 }
 
 TEST_F(GPTIMERTest, TimerTicksPrescalerUnderflow)
 {
-    GPTIMER timer;
+    GPTIMER timer(8, 0x31);
 
     timer.Write(0x0, 0x1); // Set scaler to 1
     timer.Write(0x4, 0x1); // Set prescaler to 1
-    ASSERT_EQ(timer.Read(0x10) ,0xffffffff);
-    ASSERT_EQ(timer.Read(0x14) ,0xffffffff); // rld minus one
+    ASSERT_EQ(timer.Read(0x10) ,0x270f);
+    ASSERT_EQ(timer.Read(0x14) ,0x270f);
     ASSERT_EQ(timer.Read(0x18) & 0x1, 0x1);
     ASSERT_EQ(timer.Read(0x04) ,0x1);
  
@@ -106,19 +106,20 @@ TEST_F(GPTIMERTest, TimerTicksPrescalerUnderflow)
         timer.Tick();
     
     // Read counters of timers, ony first one shuold be decremented
-    ASSERT_EQ(timer.Read(0x10), 0xfffffffe);
-    ASSERT_EQ(timer.Read(0x20), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x30), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x40), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x50), 0xffffffff);
-    u32 r7 = timer.Read(0x60); ASSERT_EQ(r7, 0xffffffff);
-    ASSERT_EQ(timer.Read(0x70), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x10), 0x270e);
+    ASSERT_EQ(timer.Read(0x20), 0x270f);
+    ASSERT_EQ(timer.Read(0x30), 0x270f);
+    ASSERT_EQ(timer.Read(0x40), 0x270f);
+    ASSERT_EQ(timer.Read(0x50), 0x270f);
+    u32 r7 = timer.Read(0x60); ASSERT_EQ(r7, 0x270f);
+    ASSERT_EQ(timer.Read(0x70), 0x270f);
 }
 
 
 TEST_F(GPTIMERTest, TimerEnableOthers)
 {
-    GPTIMER timer;
+    GPTIMER timer(8, 0x31);
+
 
     timer.Write(0x0, 0x1); // Set scaler to 1
     timer.Write(0x4, 0x1); // Set prescaler to 1
@@ -127,8 +128,8 @@ TEST_F(GPTIMERTest, TimerEnableOthers)
     timer.Write(0x8, ctrl | (0b10101 << 16)); // Enable timer 0, 2, 4
     timer.Reset();
 
-    ASSERT_EQ(timer.Read(0x10) ,0xffffffff);
-    ASSERT_EQ(timer.Read(0x14) ,0xffffffff); // rld minus one
+    ASSERT_EQ(timer.Read(0x10) ,0x270f);
+    ASSERT_EQ(timer.Read(0x14) ,0x270f); // rld minus one
     ASSERT_EQ(timer.Read(0x18) & 0x1, 0x1);
     ASSERT_EQ(timer.Read(0x04) ,0x1);
  
@@ -138,13 +139,13 @@ TEST_F(GPTIMERTest, TimerEnableOthers)
         timer.Tick();
     
     // Read counters of timers, ony first, third and fifth  shuold be decremented
-    ASSERT_EQ(timer.Read(0x10), 0xfffffffe);
-    ASSERT_EQ(timer.Read(0x20), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x30), 0xfffffffe);
-    ASSERT_EQ(timer.Read(0x40), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x50), 0xfffffffe);
-    u32 r7 = timer.Read(0x60); ASSERT_EQ(r7, 0xffffffff);
-    ASSERT_EQ(timer.Read(0x70), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x10), 0x270e);
+    ASSERT_EQ(timer.Read(0x20), 0x270f);
+    ASSERT_EQ(timer.Read(0x30), 0x270e);
+    ASSERT_EQ(timer.Read(0x40), 0x270f);
+    ASSERT_EQ(timer.Read(0x50), 0x270e);
+    u32 r7 = timer.Read(0x60); ASSERT_EQ(r7, 0x270f);
+    ASSERT_EQ(timer.Read(0x70), 0x270f);
 
 }
 
@@ -172,12 +173,12 @@ TEST_F(GPTIMERTest, TimerIRQOnUnderflow)
         timer.Tick();
    
     ASSERT_EQ(timer.Read(0x10), 0x0);
-    ASSERT_EQ(timer.Read(0x20), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x20), 0x270f);
     ASSERT_EQ(timer.Read(0x30), 0x0);
-    ASSERT_EQ(timer.Read(0x40), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x40), 0x270f);
     ASSERT_EQ(timer.Read(0x50), 0x0);
-    ASSERT_EQ(timer.Read(0x60), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x70), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x60), 0x270f);
+    ASSERT_EQ(timer.Read(0x70), 0x270f);
 
     // Tick again twice, to empty prescaler.
     // Should relaoud counter, not generate IRQ    
@@ -185,13 +186,13 @@ TEST_F(GPTIMERTest, TimerIRQOnUnderflow)
     timer.Tick();
 
 
-    ASSERT_EQ(timer.Read(0x10), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x20), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x30), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x40), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x50), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x60), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x70), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x10), 0x270f);
+    ASSERT_EQ(timer.Read(0x20), 0x270f);
+    ASSERT_EQ(timer.Read(0x30), 0x270f);
+    ASSERT_EQ(timer.Read(0x40), 0x270f);
+    ASSERT_EQ(timer.Read(0x50), 0x270f);
+    ASSERT_EQ(timer.Read(0x60), 0x270f);
+    ASSERT_EQ(timer.Read(0x70), 0x270f);
     ASSERT_FALSE(timer.CheckInterrupt());
 
 
@@ -211,6 +212,7 @@ TEST_F(GPTIMERTest, TimerIRQOnUnderflow)
    
     
     ASSERT_TRUE(timer.CheckInterrupt(false));
+
 
     // Clear interrupt by setting IP bit to 1
     u32 TCTRL = timer.Read(0x38);
@@ -266,19 +268,19 @@ TEST_F(GPTIMERTest, TimerExplicitReload)
         timer.Tick();
    
     ASSERT_EQ(timer.Read(0x10), 0x10000);
-    ASSERT_EQ(timer.Read(0x20), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x20), 0x270f);
     ASSERT_EQ(timer.Read(0x30), 0x10000);
-    ASSERT_EQ(timer.Read(0x40), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x40), 0x270f);
     ASSERT_EQ(timer.Read(0x50), 0x10000);
-    ASSERT_EQ(timer.Read(0x60), 0xffffffff);
-    ASSERT_EQ(timer.Read(0x70), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x60), 0x270f);
+    ASSERT_EQ(timer.Read(0x70), 0x270f);
 
     // Reload counter for timer 4:
     u32 TCTRL = timer.Read(0x58);
     ASSERT_EQ((TCTRL >> 2) & 0xf, 0x0);
     TCTRL |= (0x1 << 2); // SET LD bit to 1
     timer.Write(0x58, TCTRL);
-    ASSERT_EQ(timer.Read(0x50), 0xffffffff);
+    ASSERT_EQ(timer.Read(0x50), 0x270f);
     ASSERT_EQ((timer.Read(0x58) >> 2) & 0x1, 0x0); // ld bit should be cleared 
 
 
