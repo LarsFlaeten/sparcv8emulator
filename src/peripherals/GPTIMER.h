31689653a97f96ab3fe10b1b4c8ad483a4b5d1c9
LarsFlaeten <lars.flaeten@gmail.com>
2025-02-19 19:42:45 +0100

    Correct behavoir of timer tick when not enabled, and correct bhvr when interrupts disabled. Remove print debugging

--- src/peripherals/GPTIMER.h
+++ src/peripherals/GPTIMER.h
@@ -1,6 +1,7 @@
 #ifndef _GPTIMER_H_
 #define _GPTIMER_H_
 
+#define NUM_IMPLEMENTED_TIMERS 0x2
 
 class GPTIMER {
     private:
@@ -17,7 +18,7 @@ class GPTIMER {
             u32 TCTRL;
             u32 TLATCH;
 
-            timer_impl() : TRLDVAL(0x270f) {
+            timer_impl() : TCNTVAL(0x270f), TRLDVAL(0x270f), TCTRL(0), TLATCH(0) {
                 // Default is RS bit set, not IRQ, disabled
                 TCTRL = (0x1 << 1);
             }
@@ -47,22 +48,43 @@ class GPTIMER {
                     // RELOAD if RS bit:
                     if((TCTRL >> 1) & 0x1)
                         TCNTVAL = TRLDVAL;
+                    else {
+                        // No relaod, stop at -1 and disable
+                        --TCNTVAL;
+                        TCTRL = TCTRL & ~0x1;
+                    }
                 } else {
                     --TCNTVAL;
                 }
             }
         };
 
-        timer_impl timers[7];
+        timer_impl timers[NUM_IMPLEMENTED_TIMERS];
     public:
         u32 VendorId() const { return 0x01; }
         u32 DeviceId() const { return 0x011;}
     
-        GPTIMER(u32 IRQ = 8, u32 prescaler = 0x31) : SRELOAD(prescaler) {
-            CONFIG = (0x1 << 16) | (IRQ & 0x1f) << 3 | 0x7; // only one timer enabled, 7 implemented
+        GPTIMER(u32 IRQ = 8, u32 prescaler = 0x31) : SRELOAD(prescaler), CATCHCFG(0) {
+            //CONFIG = (0x1 << 16) | (IRQ & 0x1f) << 3 | 0x7; // only one timer enabled, 7 implemented
+            CONFIG = (0x1 << 8) | (IRQ & 0x1f) << 3 | NUM_IMPLEMENTED_TIMERS; // none enabled, separate IRQs, 2 implemented
+            
             SRELOAD = prescaler & 0xffff;
 
             Reset();
+        }
+
+        void SetLEONState() {
+            // Set LEON specific startup state:
+            // Timer 1 enabled, timer 2 disabled and all zeroes
+            timers[0].TCTRL = 0x3; // RS bit and enable bit
+            timers[0].TCNTVAL = 0xffffffff;
+            timers[0].TRLDVAL = 0xffffffff;
+            timers[1].TCTRL = 0;
+            timers[1].TCNTVAL = 0;
+            timers[1].TRLDVAL = 0;
+    
+            SCALER = 0x24;            
+
         } 
 
         bool CheckInterrupt(bool clear = true) {
@@ -96,41 +118,63 @@ class GPTIMER {
         }
 
         void Tick() {
-            if (SCALER == 0) {
-                for (auto & timer : timers) {
-                    if(timer.IsEnabled())
-                        timer.Tick();
-                }   
-                SCALER = SRELOAD & 0xffff; 
-            } else {
-                --SCALER;
+            // only propagate tick when at leats one timer is enabled:
+            // CONFIG:TIMERN field (bits 16 -22)
+            //if(((CONFIG >> 16) & 0x7f) > 0 )
+            // FIX Do not use CONFIG field, this is write only
+            if(timers[0].IsEnabled() || timers[1].IsEnabled())
+            {
+            
+                if (SCALER == 0) {
+                    for (auto & timer : timers) {
+                        if(timer.IsEnabled())
+                            timer.Tick();
+                    }   
+                    SCALER = SRELOAD & 0xffff; 
+                } else {
+                    --SCALER;
+                }
             }
         }
 
         u32 Read(u32 offset) const {
+            u32 ret;
             switch(offset) {
-                case(0x0): return SCALER;
-                case(0x4): return SRELOAD;
-                case(0x8): return CONFIG;
-                case(0xc): return CATCHCFG;
+                case(0x0): ret = SCALER; break;
+                case(0x4): ret = SRELOAD; break;
+                case(0x8): ret = CONFIG & ~(0x7f << 16); break;
+                case(0xc): ret = CATCHCFG; break;
                 default:
                     if(offset <= 0x7c) {
                         int n = (offset - 0x10) / 0x10;
+                        
+                        if(n > NUM_IMPLEMENTED_TIMERS - 1) {
+                            ret = 0;
+                            break;
+                        }
+                        
                         int o = offset % 0x10;
                         switch(o) {
-                            case(0x0): return timers[n].TCNTVAL;
-                            case(0x4): return timers[n].TRLDVAL;
-                            case(0x8): return timers[n].TCTRL;
-                            case(0xc): return timers[n].TLATCH;
-                            default: return 0;
+                            case(0x0): ret = timers[n].TCNTVAL; break;
+                            case(0x4): ret = timers[n].TRLDVAL; break;
+                            case(0x8): ret = timers[n].TCTRL; break;
+                            case(0xc): ret = timers[n].TLATCH; break;
+                            default: ret = 0; break;
                         }
                     } else {
-                        return 0x0;
+                        ret = 0x0;
                     }
+                    break;
             }
+            
+            //std::cout << "read GPTIMER at offset " << std::hex << offset << ", value= " << ret << "\n";
+            return ret;
+            
         }
 
         void Write(u32 offset, u32 regvalue) {
+            //std::cout << "write GPTIMER at offset " << std::hex << offset << ", value= " << regvalue << "\n";
+            
             switch(offset) {
                 case(0x0): SCALER = regvalue; return;
                 case(0x4): SRELOAD = regvalue; return;
@@ -139,14 +183,26 @@ class GPTIMER {
                 default:
                     if(offset <= 0x7c) {
                         int n = (offset - 0x10) / 0x10;
+                        if(n > NUM_IMPLEMENTED_TIMERS - 1) {
+                            return; // Writes beyond implemented timers have no effect
+                        }
                         int o = offset % 0x10;
                         switch(o) {
                             case(0x0): timers[n].TCNTVAL = regvalue; return;
                             case(0x4): timers[n].TRLDVAL = regvalue; return;
                             case(0x8): 
-                                // If IP bit is set, clear it in the reg, GRIP 36.3.7:
-                                if( (regvalue >> 4) & 0x1 )
+                                // adjustment of timers[n].TCTRL
+                                // If EN bit is set, also set correspongin bit in CONTROL:
+                                if( (regvalue) & 0x1 )
+                                    CONFIG = CONFIG | (0x1 << (16 + n));
+                               if( ((regvalue >> 4) & 0x1) == 0x1){
+                                    // If IP bit is set, clear it in the reg, GRIP 36.3.7:
                                     regvalue = regvalue & ~(0x1 << 4);
+                                } else {
+                                    // Writes of '0' has no effect, GRIP 36.3.7:
+                                    // I.e. keep bit in TCTRL
+                                    regvalue = regvalue | (timers[n].TCTRL & (0x1 << 4));
+                                }
                                 // IF LD bit is set, reload cnt and clear the bit
                                 if( (regvalue >> 2) & 0x1 ) {
                                     regvalue = regvalue & ~(0x1 << 2);
