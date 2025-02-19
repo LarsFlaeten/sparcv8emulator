5e36517535d36c48eeb9032120e816f7b9f5a4af
LarsFlaeten <lars.flaeten@gmail.com>
2025-02-19 19:40:34 +0100

    Rewrite tests to account for PIMASK

--- test/testIRQMP.cpp
+++ test/testIRQMP.cpp
@@ -59,13 +59,22 @@ TEST_F(IRQMPTest, TriggerIRQ_CheckPending)
     IRQMP intc;
 
     intc.TriggerIRQ(8);
-
     u32 ipend = intc.Read(0x04);
+    ASSERT_FALSE(ipend & 0x1 << 8);
+
+    // Enable MP interrupt mask for IRQ 8:
+    intc.Write(0x40, 0b1 << 8);
+
+    intc.TriggerIRQ(8);
+ 
+    ipend = intc.Read(0x04);
     ASSERT_TRUE(ipend & 0x1 << 8);
 
     unsigned int irq = intc.GetNextIRQPending();
     ASSERT_EQ(irq, 8);
-
+    intc.ClearIRQ(8);
+    ASSERT_EQ(intc.GetNextIRQPending(), 0);
+ 
    
     intc.Write(0x04, 0x0); 
     ASSERT_EQ(intc.GetNextIRQPending(), 0);
@@ -74,7 +83,20 @@ TEST_F(IRQMPTest, TriggerIRQ_CheckPending)
     intc.TriggerIRQ(8);
     intc.TriggerIRQ(11);
 
+    // The mask will ensure only IRQ8 is pending:
+    ASSERT_EQ(intc.GetNextIRQPending(), 8);
+    intc.ClearIRQ(8);
+    ASSERT_EQ(intc.GetNextIRQPending(), 0);
+
     
+    // Enable MP interrupt mask for IRQ 3,8,11:
+    intc.Write(0x40, 0x1 << 3 | 0x1 << 8 | 0x1 << 11);
+
+    intc.TriggerIRQ(3);
+    intc.TriggerIRQ(8);
+    intc.TriggerIRQ(11);
+
+   
     ASSERT_EQ(intc.GetNextIRQPending(), 11);
     intc.ClearIRQ(11);
 
