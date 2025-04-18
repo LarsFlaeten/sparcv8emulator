d404c58932a922b415ecffaed8fa936fa78cefec
LarsFlaeten <lars.flaeten@gmail.com>
2025-04-18 19:19:40 +0200

    A small test, related to LEON power function and sleeping

---
+++ src/timer_test.cpp
@@ -0,0 +1,30 @@
+
+
+
+#include "LoopTimer.h"
+
+
+int main(int argc, char **argv)
+{
+        LoopTimer lt;
+        lt.start(); 
+        
+        int i = 0;
+        struct timespec req = {0, 10000};  // 10 ms = 10,000,000 ns
+        
+       
+        while(i < 1000) {
+            lt.start();
+            //nanosleep(&req, NULL);
+            ++i;
+            lt.stop();
+        }
+
+        lt.printStats();
+
+
+
+
+
+}
+
