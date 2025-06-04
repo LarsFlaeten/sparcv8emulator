5b6c66979b5264c5672efc41c3aded792dad16ee
LarsFlaeten <lars.flaeten@gmail.com>
2025-06-04 08:11:11 +0200

    Move test Macros to test.h

--- test/asm/libsparc.h
+++ test/asm/libsparc.h
@@ -33,21 +33,6 @@ void startup();
 void exit (int status);
 
 
-// Use these macros in c-code main() to put expected values fro failure and success in g7
-// The macros will abort and report sucess/faulre if used in main()
-#define FAIL_TEST()            	\
-    do {                       	\
-     	int regval = 0xbad;    	\
-    	asm volatile ("mov %0, %%g7\n\t" : : "r" (regval) : "memory"); \
-		return 1;					\
-    } while (0)
-
-#define SUCEED_TEST()           \
-    do {                       	\
-     	int regval = 0x900d;   	\
-    	asm volatile ("mov %0, %%g7\n\t" : : "r" (regval) : "memory"); \
-		return 0;					\
-    } while (0)
 
 
 #endif
